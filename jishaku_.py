ab22dbd12ff79320d749ae2673a6943c269da662
Puncher1 <mcocexpert@hispeed.ch>
2021-07-22 22:58:57 +0200

    Remove underscore prefix

--- jishaku_.py
+++ jishaku_.py
@@ -1,11 +1,14 @@
 import discord
 
+import os
 import jishaku
 # end imports
 
 
 async def jishaku_func(client):
+    os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
     client.load_extension("jishaku")
+
 
 
 
