02e2e66ee8612bef9b23878d5520ff77193b424e
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-07-24 01:01:27 +0200

    Added subclasses

---
+++ Python/subclasses.py
@@ -0,0 +1,96 @@
+
+# Simple class inheritance
+class Parent:
+
+    weight = 70
+
+    def __init__(self, first, last, height):
+        self.first = first                                  # initialize the instance's attributes
+        self.last = last
+        self.height = height
+
+    def fullname(self):
+        return f"{self.first} {self.last}"
+
+    def body_mass_index(self):
+        return self.weight / (self.height * self.height)
+
+
+class Child(Parent):                                        # copy/inherit class Parent to class Child
+    pass                                                    # pass: nothing added or changed
+
+
+child_1 = Child("Max", "Mustermann", 1.80)
+child_2 = Parent("Max", "Mustermann", 1.80)
+
+print(child_1.weight)
+print(child_1.fullname())
+print(child_1.body_mass_index(), child_2.body_mass_index())
+print(child_1.body_mass_index() == child_2.body_mass_index())
+
+print("-------------------------")
+# ---------------------------------------------------------------------------
+
+
+# Change var and add functions in subclasses
+class Employee:
+
+    min_salary = 5000
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        self.email = f"{first}.{last}@email.com"
+
+    def fullname(self):
+        return f"{self.first} {self.last}"
+
+
+class Developer(Employee):                                  # inherit class Employee to class Developer
+    min_salary = 5000 + 1000                                # changing 'min_salary' var
+
+
+class Manager(Employee):
+    min_salary = 5000 + 2000
+
+    def __init__(self, first, last, employees=None):        # init function with new parameter 'employees' (None as default)
+        super().__init__(first, last)                       # super() makes that class Employee handles these vars which are given in the parameters
+
+        if employees is None:
+            self.employees = []                             # 'employees' is the new attr
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print("-->", emp.fullname())
+
+
+emp_1 = Employee("Felix", "Laden")
+print(emp_1.min_salary)
+
+dev_1 = Developer("Max", "Müller")
+dev_2 = Developer("Hannah", "Scherz")
+print(dev_1.min_salary)
+
+mgr_1 = Manager("Erich", "Heinz")
+mgr_2 = Manager("Julia", "Washington", [dev_1, dev_2])
+
+mgr_1.add_emp(dev_1)
+mgr_1.print_emps()
+print("*")
+
+mgr_2.print_emps()
+print("*")
+mgr_2.remove_emp(dev_1)
+mgr_2.print_emps()
+
+print("-------------------------")
