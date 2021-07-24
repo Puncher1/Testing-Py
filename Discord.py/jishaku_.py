ee4bef2443aaa1c0f03ff8bcb31572ce879c4706
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-07-24 20:30:27 +0200

    Fixed jishaku didn't work

--- Discord.py/jishaku_.py
+++ Discord.py/jishaku_.py
@@ -1,13 +1,17 @@
 import discord
+from discord.ext import commands
 
 import os
 import jishaku
 # end imports
 
 
-async def jishaku_func(client):
+async def jishaku_func(client: commands.Bot):
     os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+    os.environ['JISHAKU_NO_DM_TRACEBACK'] = "True"
+    os.environ['JISHAKU_RETAIN'] = 'True'
     client.load_extension("jishaku")
+    client.reload_extension("jishaku")
 
 
 
