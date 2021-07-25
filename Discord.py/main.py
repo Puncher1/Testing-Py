d5ff0cf241501ef09b7dc6ef59cd724742c426d4
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-07-25 02:21:00 +0200

    Added about command

--- Discord.py/main.py
+++ Discord.py/main.py
@@ -1,11 +1,13 @@
 import discord
 from discord.ext import commands
-
 import os
+import sys
 from dotenv import load_dotenv
 import asyncio
 
 import jishaku_
+import help_cmd
+import github_api
 # end imports
 
 
@@ -14,22 +16,44 @@ intents = discord.Intents.all()
 client = commands.Bot(command_prefix="$", intents=intents)
 # end setup
 
+BOT_TOKEN = os.getenv("TEST_TOKEN")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
-async def invoking_func():
+PUNCHER_ID = 305354423801217025
+# end global constants
+
+
+async def invoking_funcs():
     asyncio.create_task(jishaku_.jishaku_func(client))
 
 
 @client.event
 async def on_ready():
-    await invoking_func()
+    print("Ready!")
+    await invoking_funcs()
 # end invoking functions
 
 
 @client.command(aliases=["s"])
 @commands.is_owner()
 async def shutdown(ctx):
-    await client.logout()
+    await client.close()
 # end shutdown
+
+@client.command()
+async def about(ctx):
+    github_desc = await github_api.github_api(GITHUB_TOKEN)
+
+    owner = client.get_user(PUNCHER_ID)
+
+    embed = discord.Embed()
+    embed.title = f"About {client.user.name}"
+    embed.description = f"{client.user.name} is a bot for development test purposes for {owner.name}." \
+                        f"\nPython {sys.version[0:3]} Discord.py {discord.__version__}"
+    embed.add_field(name="Developer", value=f"```{owner}```", inline=False)
+    embed.add_field(name="GitHub", value=f"{github_desc}", inline=False)
+    await ctx.send(embed=embed)
+
 
 client.run(os.getenv("TEST_TOKEN"))
 # end main file
