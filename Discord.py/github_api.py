592976ac3954054519c4c3d147206b909cfaed01
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-07-25 02:22:04 +0200

    Added github_api

---
+++ Discord.py/github_api.py
@@ -0,0 +1,23 @@
+import requests
+from github import Github
+
+
+async def github_api(github_token):
+    GITHUB_TOKEN = github_token
+    # end local constants
+
+    g = Github(GITHUB_TOKEN)
+    repo = g.get_repo("Puncher1/Testing")
+
+    main_branch = repo.get_branch("main")
+    commit = main_branch.commit.commit
+
+    html_url = commit.html_url
+    author = commit.author
+    message = commit.message
+    sha_short = commit.sha[:7]
+
+    string = f"[[Testing:main] Last commit](https://github.com/Puncher1/Testing)" \
+             f"\n[`{sha_short}`]({html_url}) {message} - {author.name}"
+
+    return string
